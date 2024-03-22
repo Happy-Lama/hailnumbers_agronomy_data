892374654f183e9634680754e15b1154a623731d
Happy-Lama <kldenis2001@gmail.com>
2024-03-22 13:01:15 +0300

    build settings

--- app/app/settings.py
+++ app/app/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -88,6 +89,12 @@ DATABASES = {
     }
 }
 
+if RENDER_EXTERNAL_HOSTNAME:
+    DATABASES['default'] = dj_database_url.config(
+            default=os.environ.get("DATABASE_URL"),
+            conn_max_age=600
+        )
+    
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
