fe19cce42ede4057dc83ae35162d056984ac2b80
Happy-Lama <kldenis2001@gmail.com>
2024-03-30 13:00:13 +0300

    Added timestamp

--- app/api/models.py
+++ app/api/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.utils import timezone 
 
 # Create your models here.
 class SoilParameters(models.Model):
+    timestamp = models.DateTimeField(default=timezone.now)
     soil_temperature = models.FloatField(default=0)
     soil_moisture = models.FloatField(default=0)
     soil_ph = models.FloatField(default=0)
--- app/api/serializers.py
+++ app/api/serializers.py
@@ -5,4 +5,4 @@ class SoilParametersSerializer(serializers.ModelSerializer):
     class Meta:
         model = SoilParameters
         fields = '__all__'
-        # fields = ['soil_temperature', 'soil_moisture', 'soil_ph', 'soil_nitrogen_content', 'soil_phosphorus_content', 'soil_potassium_content', 'soil_electrical_conductivity']
+        
